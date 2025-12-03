32177f37ef659c17528c56e31c8d27f708159b9f
its-hmtu <tu.hoangminh15@gmail.com>
2025-12-03 16:21:13 +0000

    update gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,9 @@ import ButtonToTop from "@/components/ui/button-to-top";
 // import Chat from "./components/chat";
 import ContacNow from "./components/contac-now";
 // import Script from "next/script";
-import Head from "next/head";
-
+import {
+  GoogleTagManager
+} from '@next/third-parties/google';
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,31 +17,9 @@ export default function RootLayout({
   const gtmId = 'GTM-M7CJMKX6';
   return (
     <html lang="en">
-      <Head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `<!-- Google Tag Manager -->
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', ${gtmId});</script>
-            <!-- End Google Tag Manager -->`,
-          }}
-        />
-      </Head>
+      <GoogleTagManager gtmId={gtmId} />
       <body className="antialiased max-w-[100dvw] overflow-x-hidden">
-        
         {/* <Chat /> */}
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
         <Header />
         <ContacNow />
         {children}
